20b94d32ba58c8efa8fc681ef42d513f57ad1a5c
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2025-02-11 17:00:24 +0200

    feat(modules): Add apartments module controllers and providers

--- src/modules/apartments/apartments.module.ts
+++ src/modules/apartments/apartments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ApartmentsController } from './apartments.controller';
+import { ApartmentsService } from './apartments.service';
 import { Apartment } from '../../models/apartment.model';
 
 @Module({
   imports: [SequelizeModule.forFeature([Apartment])],
+  controllers: [ApartmentsController],
+  providers: [ApartmentsService],
 })
 export class ApartmentsModule {}
